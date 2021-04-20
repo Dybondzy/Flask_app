75f93fa3510c78fe092b4601bc06161f536d7e07
Dybondzy <79281335+Dybondzy@users.noreply.github.com>
2021-04-19 22:41:29 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 # 1. import Flask
-from flask import Flask
+
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+##################
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
@@ -20,4 +30,212 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+##################
 
+
+# Define index route
+@app.route("/")
+def index():
+    return "Welcome to my API!"
+
+
+# 5. Define the contact route
+@app.route("/contact")
+def contact():
+    email = "nick@example.com"
+
+    return f"Questions? Comments? Complaints? Shoot an email to {email}."
+
+
+@app.route("/")
+def session():
+    return "Hi **** THIS IS MY session PAGE ***********"
+
+@app.route("/normal")
+def normal():
+    return str(hello_list)
+
+@app.route("/jsonified")
+def jsonified_list():
+    #return jsonify(hello_list)
+    return jsonify(hello_dict)
+
+@app.route("/dict")
+def dictionary():
+    return hello_dict
+
+
+#################################################
+
+@app.route("/api/v1.0/justice-league")
+def justice_league():
+    """Return the justice league data as json"""
+
+    return jsonify(justice_league_members)
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Justice League API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/justice-league"
+    )
+ 
+
+@app.route("/measurement")
+def measurement():
+    return f"I am measurement."
+
+
+@app.route("/passengers")
+def passenger():
+    return f"I am passengers."
+
+
+
+@app.route("/station")
+def station():
+    return f"I am station."
+
+
+
+@app.route("/jsonified")
+def jsonified_list():
+    #return jsonify(hello_list)
+    return jsonify(hello_dict)
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/justice-league")
+def justice_league():
+    """Return the justice league data as json"""
+
+    return jsonify(justice_league_members)
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Justice League API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/justice-league<br/>"
+        f"/api/v1.0/justice-league/superhero/aquaman<br/>"
+        f"/api/v1.0/justice-league/superhero/batman<br/>"
+        f"/api/v1.0/justice-league/superhero/cyborg<br/>"
+        f"/api/v1.0/justice-league/superhero/flash<br/>"
+        f"/api/v1.0/justice-league/superhero/greenlantern<br/>"
+        f"/api/v1.0/justice-league/superhero/superman<br/>"
+        f"/api/v1.0/justice-league/superhero/wonderwoman"
+    )
+
+
+@app.route("/api/v1.0/justice-league/real_name/<real_name>")
+def justice_league_by_real_name(real_name):
+    """Fetch the Justice League character whose real_name matches
+       the path variable supplied by the user, or a 404 if not."""
+
+    canonicalized = real_name.replace(" ", "").lower()
+    for character in justice_league_members:
+        search_term = character["real_name"].replace(" ", "").lower()
+
+        if search_term == canonicalized:
+            return character
+
+    return {"error": f"Character with real_name {real_name} not found."}, 404
+
+
+@app.route("/api/v1.0/justice-league/superhero/<superhero>")
+def justice_league_by_superhero__name(superhero):
+    """Fetch the Justice League character whose superhero matches
+       the path variable supplied by the user, or a 404 if not."""
+
+    canonicalized = superhero.replace(" ", "").lower()
+    for character in justice_league_members:
+        search_term = character["superhero"].replace(" ", "").lower()
+
+        if search_term == canonicalized:
+            return character
+
+    return {"error": "Character not found."}, 404
+
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///titanic.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Passenger = Base.classes.passenger
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/api/v1.0/names")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Passenger.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+
+@app.route("/api/v1.0/passenger")
+def passenger():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+    
